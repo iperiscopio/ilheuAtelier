c2578bbf37d4769aadef32f7dfcc86c11af66686
iperiscopio <49154268+iperiscopio@users.noreply.github.com>
2023-02-26 00:52:46 +0000

    composer lock update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '35a311da9fc67248fd47f929bc34501036153187',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '35a311da9fc67248fd47f929bc34501036153187',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.7.1',
+            'version' => '6.7.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => '49cd7ea3d2563f028d7811f06864a53b1f15ff55',
+            'dev_requirement' => false,
+        ),
+        'rbdwllr/reallysimplejwt' => array(
+            'pretty_version' => '3.1.2',
+            'version' => '3.1.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../rbdwllr/reallysimplejwt',
+            'aliases' => array(),
+            'reference' => '3e849792e591fcea5bd77401ff8dce0f31e16c9a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
