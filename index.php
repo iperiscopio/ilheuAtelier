4ae8cb95a8d8a07086288dea2c274aaad3a75f94
iperiscopio <49154268+iperiscopio@users.noreply.github.com>
2023-02-26 20:45:35 +0000

    url update

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
 <style>@font-face{font-family:atipland-book;src:local("atipland-book"),url(atipland-book.cedea9a46d57577b.otf) format("truetype")}*{margin:0 auto;box-sizing:border-box;font-family:atipland-book,sans-serif;color:#0000f2}body{background-color:#f6f5ea}</style><link rel="stylesheet" href="styles.fe2209fcd902c687.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles.fe2209fcd902c687.css"></noscript></head>
 <body>
   <app-root></app-root>
-<script src="runtime.31022f6d7dafc1c1.js" type="module"></script><script src="polyfills.078f3aefb6bb3da7.js" type="module"></script><script src="main.09a4b8b2063ac44c.js" type="module"></script>
+<script src="runtime.31022f6d7dafc1c1.js" type="module"></script><script src="polyfills.078f3aefb6bb3da7.js" type="module"></script><script src="main.1145c3d4054c11f5.js" type="module"></script>
 
 </body></html>
