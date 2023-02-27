ef6ceb986ae5de77fa422b0c70d98297e6a0441b
iperiscopio <49154268+iperiscopio@users.noreply.github.com>
2023-02-27 20:17:58 +0000

    REMOTE_ADDRESS change to HTTP_X_FORWARDED_FOR

--- api/controllers/captcha.php
+++ api/controllers/captcha.php
@@ -39,7 +39,7 @@
 
     if( $_SERVER["REQUEST_METHOD"] === "GET" ) {
 
-        $userIp = $_SERVER["REMOTE_ADDR"];
+        $userIp = $_SERVER['HTTP_X_FORWARDED_FOR'];
 
         http_response_code(202);
 
@@ -49,10 +49,9 @@
 
     } else if( $_SERVER["REQUEST_METHOD"] === "POST" ){
 
-        $userIp = $_SERVER["REMOTE_ADDR"];
+        $userIp = $_SERVER['HTTP_X_FORWARDED_FOR'];
 
         $data = json_decode( file_get_contents("php://input"), true );
-        
         
         if( validate( $data ) ) {
 
