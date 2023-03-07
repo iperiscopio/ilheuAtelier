d10d48856eacbd03295040e91591ccab03bdf7ba
iperiscopio <49154268+iperiscopio@users.noreply.github.com>
2023-03-07 19:47:43 +0000

    front end changes

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
-<style>@font-face{font-family:atipland-book;src:local("atipland-book"),url(atipland-book.cedea9a46d57577b.otf) format("truetype")}*{margin:0 auto;box-sizing:border-box;font-family:atipland-book,sans-serif;color:#0000f2}body{background-color:#f6f5ea}</style><link rel="stylesheet" href="styles.fe2209fcd902c687.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles.fe2209fcd902c687.css"></noscript></head>
+<style>@font-face{font-family:atipland-book;src:local("atipland-book"),url(atipland-book.cedea9a46d57577b.otf) format("truetype")}*{margin:0 auto;box-sizing:border-box;font-family:atipland-book,sans-serif}body{background-color:#f6f5ea}</style><link rel="stylesheet" href="styles.74f9c26fd29f987e.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles.74f9c26fd29f987e.css"></noscript></head>
 <body>
   <app-root></app-root>
-<script src="runtime.31022f6d7dafc1c1.js" type="module"></script><script src="polyfills.078f3aefb6bb3da7.js" type="module"></script><script src="main.8c3daee550ce63f0.js" type="module"></script>
+<script src="runtime.31022f6d7dafc1c1.js" type="module"></script><script src="polyfills.078f3aefb6bb3da7.js" type="module"></script><script src="main.666cac345e60f28c.js" type="module"></script>
 
 </body></html>
