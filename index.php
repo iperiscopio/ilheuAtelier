bd28fb0626f10eb2145b2297dfcec27ace3f86ac
iperiscopio <49154268+iperiscopio@users.noreply.github.com>
2023-04-27 22:46:43 +0100

    fix show project images bug

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
 <style>@font-face{font-family:atipland-book;src:local("atipland-book"),url(atipland-book.cedea9a46d57577b.otf) format("truetype")}*{margin:0 auto;box-sizing:border-box;font-family:atipland-book,sans-serif}body{background-color:#f6f5ea}</style><link rel="stylesheet" href="styles.74f9c26fd29f987e.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles.74f9c26fd29f987e.css"></noscript></head>
 <body>
   <app-root></app-root>
-<script src="runtime.31022f6d7dafc1c1.js" type="module"></script><script src="polyfills.078f3aefb6bb3da7.js" type="module"></script><script src="main.baa4d4570510049f.js" type="module"></script>
+<script src="runtime.31022f6d7dafc1c1.js" type="module"></script><script src="polyfills.078f3aefb6bb3da7.js" type="module"></script><script src="main.5aed523364143da4.js" type="module"></script>
 
 </body></html>
