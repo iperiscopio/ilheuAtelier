e28749db72845eec14ba4776d74d5a79bb4625e2
iperiscopio <49154268+iperiscopio@users.noreply.github.com>
2023-04-28 22:46:47 +0100

    updated info as client request

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html><html lang="en"><head>
   <meta charset="utf-8">
-  <title>Ilhéu Atelier</title>
+  <title>ilhéu atelier</title>
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
 <style>@font-face{font-family:atipland-book;src:local("atipland-book"),url(atipland-book.cedea9a46d57577b.otf) format("truetype")}*{margin:0 auto;box-sizing:border-box;font-family:atipland-book,sans-serif}body{background-color:#f6f5ea}</style><link rel="stylesheet" href="styles.74f9c26fd29f987e.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles.74f9c26fd29f987e.css"></noscript></head>
 <body>
   <app-root></app-root>
-<script src="runtime.31022f6d7dafc1c1.js" type="module"></script><script src="polyfills.078f3aefb6bb3da7.js" type="module"></script><script src="main.92ee7d8e2df08d6b.js" type="module"></script>
+<script src="runtime.31022f6d7dafc1c1.js" type="module"></script><script src="polyfills.078f3aefb6bb3da7.js" type="module"></script><script src="main.745995a1602fbbb1.js" type="module"></script>
 
 </body></html>
