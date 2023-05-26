b85fb797ba75d436d56b4391e5d126e694988922
iperiscopio <49154268+iperiscopio@users.noreply.github.com>
2023-05-27 00:07:26 +0100

    add apoio acores file. fix font family issues. added missing images

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
-<style>@font-face{font-family:atipland-book;src:local("atipland-book"),url(atipland-book.cedea9a46d57577b.otf) format("truetype")}*{margin:0 auto;box-sizing:border-box;font-family:atipland-book,sans-serif}body{background-color:#f6f5ea}</style><link rel="stylesheet" href="styles.74f9c26fd29f987e.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles.74f9c26fd29f987e.css"></noscript></head>
+<style>@font-face{font-family:atipland-book;src:url(atipland-book.cedea9a46d57577b.otf) format("opentype")}*{margin:0 auto;box-sizing:border-box;font-family:atipland-book,sans-serif}body{background-color:#f6f5ea}</style><link rel="stylesheet" href="styles.2fe654532e4d8089.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles.2fe654532e4d8089.css"></noscript></head>
 <body>
   <app-root></app-root>
-<script src="runtime.31022f6d7dafc1c1.js" type="module"></script><script src="polyfills.078f3aefb6bb3da7.js" type="module"></script><script src="main.3217538b8a8fafc6.js" type="module"></script>
+<script src="runtime.31022f6d7dafc1c1.js" type="module"></script><script src="polyfills.078f3aefb6bb3da7.js" type="module"></script><script src="main.9e8426be581e5b17.js" type="module"></script>
 
 </body></html>
