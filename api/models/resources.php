89874ab7311535088e8fe52c80e6a58c1b0e6010
iperiscopio <49154268+iperiscopio@users.noreply.github.com>
2023-06-03 00:13:46 +0100

    Revert "debug resources directory"
    This reverts commit 764f198dafd6812c192f762a9ba0edbfadb92a79.

--- api/models/resources.php
+++ api/models/resources.php
@@ -5,7 +5,6 @@
     class Resources extends Config {
         
         public function getResource($lang) {
-            var_dump(__DIR__);
             if ($lang === 'pt-PT') {
                 return parse_ini_file(__DIR__ . '/../Resources_PT.ini');
             } else if ($lang === 'en-GB') {
