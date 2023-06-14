a5e7fe27a8ea9d61771999675ba06d0d4a267258
iperiscopio <49154268+iperiscopio@users.noreply.github.com>
2023-06-14 23:09:30 +0100

    debug resources model file

--- api/models/resources.php
+++ api/models/resources.php
@@ -9,18 +9,12 @@
             var_dump($lang);
 
             if ($lang === 'pt-PT') {
-                var_dump('entrou pt');
-                var_dump(parse_ini_file(__DIR__ . '/../Resources_PT.ini'));
                 
                 return parse_ini_file(__DIR__ . '/../Resources_PT.ini');
 
             } else if ($lang === 'en-GB') {
 
                 return parse_ini_file(__DIR__ . '/../Resources_EN.ini');
-            } else {
-                var_dump('else');
-                return parse_ini_file(__DIR__ . '/../Resources_PT.ini');
             }
-            var_dump('fora if else');
         }
     }
