0caa91ec7d1c93c0d61d08f8a7c18f75f6e1a1f8
iperiscopio <49154268+iperiscopio@users.noreply.github.com>
2023-06-17 00:40:40 +0100

    debugg pt resources file

--- api/models/resources.php
+++ api/models/resources.php
@@ -1,12 +1,17 @@
 <?php
 
     require_once("config.php");
+    require_once("property.ini.php");
 
     class Resources extends Config {
         
         public function getResource($lang) {
             
             if ($lang === 'pt-PT') {
+                if(!file_exists(__DIR__ . '/../Resources_PT.ini')){
+                    echo 'File '.__DIR__ . '/../Resources_PT.ini does not exist';
+                    die();
+                }
                 print_r(__DIR__ . '/../Resources_PT.ini');
                 print_r(parse_ini_file(__DIR__ . '/../Resources_PT.ini'));
                 
