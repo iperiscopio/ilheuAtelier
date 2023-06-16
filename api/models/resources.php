2013359857fe743ffe0f2ba60a7a7d326a468557
iperiscopio <49154268+iperiscopio@users.noreply.github.com>
2023-06-17 00:47:17 +0100

    resources file

--- api/models/resources.php
+++ api/models/resources.php
@@ -8,17 +8,10 @@
         public function getResource($lang) {
             
             if ($lang === 'pt-PT') {
-                if(!file_exists(__DIR__ . '/../Resources_PT.ini')){
-                    var_dump('File '.__DIR__ . '/../Resources_PT.ini does not exist');
-                }
-                print_r(__DIR__ . '/../Resources_PT.ini');
-                print_r(parse_ini_file(__DIR__ . '/../Resources_PT.ini'));
                 
                 return parse_ini_file(__DIR__ . '/../Resources_PT.ini');
 
             } else if ($lang === 'en-GB') {
-                print_r(__DIR__ . '/../Resources_EN.ini');
-                print_r(parse_ini_file(__DIR__ . '/../Resources_EN.ini'));
 
                 return parse_ini_file(__DIR__ . '/../Resources_EN.ini');
             }
