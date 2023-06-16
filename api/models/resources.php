00732cdc7e3729444bb09ee8139836efcab066d2
iperiscopio <49154268+iperiscopio@users.noreply.github.com>
2023-06-17 00:44:26 +0100

    debugg pt resources file

--- api/models/resources.php
+++ api/models/resources.php
@@ -9,7 +9,7 @@
             
             if ($lang === 'pt-PT') {
                 if(!file_exists(__DIR__ . '/../Resources_PT.ini')){
-                    echo 'File '.__DIR__ . '/../Resources_PT.ini does not exist';
+                    var_dump('File '.__DIR__ . '/../Resources_PT.ini does not exist');
                 }
                 print_r(__DIR__ . '/../Resources_PT.ini');
                 print_r(parse_ini_file(__DIR__ . '/../Resources_PT.ini'));
