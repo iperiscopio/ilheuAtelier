711c12a93aa703959e2be0f956b2cfcb08d20bfe
iperiscopio <49154268+iperiscopio@users.noreply.github.com>
2023-06-17 00:20:11 +0100

    renamed PT resources file

--- api/models/resources.php
+++ api/models/resources.php
@@ -8,7 +8,7 @@
             
             if ($lang === 'pt-PT') {
                 
-                return parse_ini_file(__DIR__ . '/../Resources_PT.ini');
+                return parse_ini_file(__DIR__ . '/../Resource_PT.ini');
 
             } else if ($lang === 'en-GB') {
 
