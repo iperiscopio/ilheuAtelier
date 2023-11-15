716f39fe6ffe96e2ffa0c4eb36bf7097e13a442b
iperiscopio <49154268+iperiscopio@users.noreply.github.com>
2023-11-15 23:04:38 +0000

    debugging index.php file

--- api/index.php
+++ api/index.php
@@ -14,6 +14,8 @@
     );
 
     $url_parts = explode("/", $_SERVER["REQUEST_URI"]);
+    print_r("url_parts");
+    print_r($url_parts);
     
     $controllers = [
         "accounts-manager",
@@ -33,8 +35,11 @@
     $controller = $url_parts[2];
 
     $id = !empty($url_parts[3]) ? $url_parts[3] : "";
+    print_r("url_parts[1]");
     print_r($url_parts[1]);
+    print_r("url_parts[2]");
     print_r($url_parts[2]);
+    print_r("url_parts[3]");
     print_r($url_parts[3]);
 
     if (!in_array($controller, $controllers)) {
